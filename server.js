76867666c2d95124ae2d4527efce9e89446b95ae
Thiago Gomes <thiago.g.sousa100@gmail.com>
2025-11-20 12:13:56 -0300

    Update: mensagem no console log - "server rodando...".

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from 'cors'
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.post("/usuarios", async (req, res) => {
   await prisma.user.create({
@@ -61,4 +63,6 @@ app.delete("/usuarios/:id", async (req, res) => {
   res.status(200).json({ message: "usuário deletado com Sucesso!" });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
